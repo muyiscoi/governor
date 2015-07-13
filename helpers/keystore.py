21bbcbd2cb974ee9ddc1d3ee9b4b83acf913d8d5
Mike Tonks <michael.tonks@headforwards.com>
2015-07-13 09:04:33 +0100

    Refactor keystore helper to use etcd client library

--- helpers/keystore.py
+++ helpers/keystore.py
@@ -1,6 +1,6 @@
-import urllib2, json, os, time
+import json, os, time
 import logging
-from urllib import urlencode
+import etcd
 import helpers.errors
 
 logger = logging.getLogger(__name__)
@@ -8,114 +8,120 @@ logger = logging.getLogger(__name__)
 class Etcd:
     def __init__(self, config):
         self.scope = config["scope"]
-        self.host = config["host"]
+        self.host, self.port = config["host"].split(":")
+        self.client = etcd.Client(host=self.host, port=int(self.port))
         self.ttl = config["ttl"]
 
-    def get_client_path(self, path, max_attempts=1):
+    def get(self, path, max_attempts=1):
         attempts = 0
         response = None
 
         while True:
             try:
-                response = urllib2.urlopen(self.client_url(path)).read()
+                logger.debug("GET: /service/%s%s", self.scope, path)
+                response = self.client.read("/service/%s%s" % (self.scope, path))
                 break
-            except (urllib2.HTTPError, urllib2.URLError) as e:
+            except (etcd.EtcdKeyNotFound) as e:
                 attempts += 1
                 if attempts < max_attempts:
                     logger.info("Failed to return %s, trying again. (%s of %s)" % (path, attempts, max_attempts))
                     time.sleep(3)
                 else:
                     raise e
-        try:
-            return json.loads(response)
-        except ValueError:
-            return response
 
-    def put_client_path(self, path, data):
-        opener = urllib2.build_opener(urllib2.HTTPHandler)
-        request = urllib2.Request(self.client_url(path), data=urlencode(data).replace("false", "False"))
-        request.get_method = lambda: 'PUT'
-        opener.open(request)
+        return (response.value or response)
 
-    def client_url(self, path):
-        return "http://%s/v2/keys/service/%s%s" % (self.host, self.scope, path)
+    def set(self, path, value, ttl=None, prevExist=None, prevValue=None):
+
+        additional_params = {}
+        if ttl is not None:
+            additional_params['ttl'] = ttl
+        if prevExist is not None:
+            additional_params['prevExist'] = prevExist
+        if prevValue is not None:
+            additional_params['prevValue'] = prevValue
+
+        logger.debug("SET: /service/%s%s > %s", self.scope, path, value)
+        self.client.write("/service/%s%s" % (self.scope, path),
+                          value, **additional_params)
 
     def current_leader(self):
         try:
-            hostname = self.get_client_path("/leader")["node"]["value"]
-            address = self.get_client_path("/members/%s" % hostname)["node"]["value"]
-
+            hostname = self.get("/leader")
+            address = self.get("/members/%s" % hostname)
             return {"hostname": hostname, "address": address}
-        except urllib2.HTTPError as e:
-            if e.code == 404:
-                return None
+
+        except etcd.EtcdKeyNotFound:
+            return None
+
+        except Exception:
             raise helpers.errors.CurrentLeaderError("Etcd is not responding properly")
 
     def members(self):
         try:
             members = []
-
-            r = self.get_client_path("/members?recursive=true")
-            for node in r["node"]["nodes"]:
-                members.append({"hostname": node["key"].split('/')[-1], "address": node["value"]})
+            members_dir = self.get("/members")
+            if members_dir:
+                for member in members_dir.children:
+                    members.append({"hostname": member.key.split('/')[-1], "address": member.value})
 
             return members
-        except urllib2.HTTPError as e:
-            if e.code == 404:
-                return None
+
+        except etcd.EtcdKeyNotFound:
+            return None
+
+        except Exception:
             raise helpers.errors.CurrentLeaderError("Etcd is not responding properly")
 
     def touch_member(self, member, connection_string):
-        self.put_client_path("/members/%s" % member, {"value": connection_string, "ttl": self.ttl})
+        self.set("/members/%s" % member, connection_string, ttl=self.ttl)
 
     def take_leader(self, value):
-        return self.put_client_path("/leader", {"value": value, "ttl": self.ttl}) == None
+        self.set("/leader", value, ttl=self.ttl)
 
     def attempt_to_acquire_leader(self, value):
         try:
-            return self.put_client_path("/leader", {"value": value, "ttl": self.ttl, "prevExist": False}) == None
-        except urllib2.HTTPError as e:
-            if e.code == 412:
-                logger.info("Could not take out TTL lock: %s" % e)
+            self.set("/leader", value, ttl=self.ttl, prevExist=False)
+            return True
+
+        except etcd.EtcdAlreadyExist:
+            logger.info("Could not aquire leader: already exists")
             return False
 
     def update_leader(self, state_handler):
         try:
-            self.put_client_path("/leader", {"value": state_handler.name, "ttl": self.ttl, "prevValue": state_handler.name})
-            self.put_client_path("/optime/leader", {"value": state_handler.last_operation()})
-        except urllib2.HTTPError:
+            self.set("/leader", state_handler.name, ttl=self.ttl, prevValue=state_handler.name)
+            self.set("/optime/leader", state_handler.last_operation())
+        except ValueError:
             logger.error("Error updating leader lock and optime on ETCD for primary.")
             return False
 
     def last_leader_operation(self):
         try:
-            return int(self.get_client_path("/optime/leader")["node"]["value"])
-        except urllib2.HTTPError as e:
-            if e.code == 404:
-                logger.error("Error updating TTL on ETCD for primary.")
-                return None
+            return int(self.get("/optime/leader"))
+
+        except etcd.EtcdKeyNotFound:
+            logger.error("Error reading TTL on ETCD for primary.")
+            return None
 
     def leader_unlocked(self):
         try:
-            self.get_client_path("/leader")
-            return False
-        except urllib2.HTTPError as e:
-            if e.code == 404:
-                return True
-            return False
-        except ValueError as e:
+            self.get("/leader")
             return False
 
+        except etcd.EtcdKeyNotFound:
+            return True
+
+        return False
+
     def am_i_leader(self, value):
-        #try:
-           reponse = self.get_client_path("/leader")
-           logger.info("Lock owner: %s; I am %s" % (reponse["node"]["value"], value))
-           return reponse["node"]["value"] == value
-        #except Exception as e:
-            #return False
+        leader = self.get("/leader")
+        logger.info("Lock owner: %s; I am %s", leader, value)
+        return leader == value
 
     def race(self, path, value):
         try:
-            return self.put_client_path(path, {"prevExist": False, "value": value}) == None
-        except urllib2.HTTPError:
+            self.set(path, value, prevExist=False)
+            return True
+        except etcd.EtcdAlreadyExist:
             return False
